1a140d07678b0be13b6506cbbbc6c1281c42e92d
usmanaftab1234 <aftab.usman@funavry.com>
2024-05-05 20:18:41 +0500

    ads.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,46 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html lang="en" translate='no'>
-                <Head />
-                <body className='bg-slate-100 dark:bg-slate-900 transition-all'>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en" translate="no">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, minimum-scale=1"
+          />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="google" content="notranslate" />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5417884017876926"
+            crossOrigin="anonymous"
+          ></script>
+          <meta
+            name="google-adsense-account"
+            content="ca-pub-5417884017876926"
+          ></meta>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5417884017876926"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
+
+        <body className="bg-slate-100 dark:bg-slate-900 transition-all">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
